83ac75981d941442c609c3452bcb17fd2686f8cb
Maciej <MaciejJ1999@interia.pl>
2023-04-07 20:53:37 +0200

    stage 0; setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    bool IsPrime(BigInteger Num)
+    {
+        if (Num < 2) return false;
+        else if (Num < 4) return true;
+        else if (Num % 2 == 0) return false;
+        else for (BigInteger u = 3; u < Num / 2; u += 2)
+        if (Num % u == 0) return false;
+        return true;
+    }
+}
